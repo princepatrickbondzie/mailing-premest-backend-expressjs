0fa4b446b268d470f24be635ff33d6af75446ed3
Prince Patrick Bondzie <ngekow@gmail.com>
2022-01-27 15:55:46 +0000

    installed and imported cors

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 require("dotenv").config();
 require("./config/database");
+const cors = require("cors");
 const express = require("express");
 const app = express();
 
 const userRoutes = require("./routes/userRoutes");
 const mailRoutes = require("./routes/mailRoutes");
 
+app.use(cors());
 app.use(express.json());
 
-app.use("/api",
-    userRoutes,
-    mailRoutes
-);
+app.use("/api", userRoutes, mailRoutes);
 
 app.listen(9000, () => console.log("Server connected smoothly..."));
